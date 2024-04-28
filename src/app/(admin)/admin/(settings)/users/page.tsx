48917720d884eb64481927da77ae751604740cf9
Theinmwe Naing <theinmwenaing22@gmail.com>
2024-04-28 11:33:26 +0630

    update users

--- src/app/(admin)/admin/(settings)/users/page.tsx
+++ src/app/(admin)/admin/(settings)/users/page.tsx
@@ -1,6 +1,7 @@
 import {
   Table,
   TableBody,
+  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
@@ -8,14 +9,47 @@ import {
 } from "@/components/ui/table";
 import UserActionButtons from "./user-action-buttons";
 import Pagination from "@/components/ui/pagination";
+import UserFilter from "./users-filter";
+import { API_URL } from "@/lib/constants";
+import { cookies } from "next/headers";
+import { validateResponse } from "@/lib/validate-response";
+import { Page, User } from "@/lib/models";
+import { buildQueryParams } from "@/lib/utils";
 
-export default function Users() {
+interface Props {
+  searchParams: { [key: string]: string | undefined };
+}
+
+const getUsers = async ({ searchParams }: Props) => {
+  const query = buildQueryParams({ ...searchParams, limit: 10 });
+
+  const url = `${API_URL}/admin/users${query}`;
+
+  const resp = await fetch(url, {
+    headers: {
+      Cookie: cookies().toString(),
+    },
+    cache: "no-store",
+  });
+
+  await validateResponse(resp);
+
+  return (await resp.json()) as Page<User>;
+};
+
+export default async function Users(props: Props) {
+  const data = await getUsers(props);
+
   return (
     <>
       <div className="flex justify-between mb-4">
         <h2>Users</h2>
       </div>
+      <UserFilter />
       <Table>
+        {data.contents.length === 0 && (
+          <TableCaption className="text-start">No users found</TableCaption>
+        )}
         <TableHeader>
           <TableRow>
             <TableHead className="uppercase min-w-[300px] w-full">
@@ -26,21 +60,23 @@ export default function Users() {
           </TableRow>
         </TableHeader>
         <TableBody className="border-b">
-          <TableRow>
-            <TableCell>
-              <h6>NNaing</h6>
-            </TableCell>
-            <TableCell className="text-sliver text-sm">
-              theinmwenaing22@gmail.com
-            </TableCell>
-            <TableCell>
-              <UserActionButtons />
-            </TableCell>
-          </TableRow>
+          {data.contents.map((u, i) => {
+            return (
+              <TableRow key={u.id}>
+                <TableCell>
+                  <h6>{u.username}</h6>
+                </TableCell>
+                <TableCell className="text-sliver text-sm">{u.email}</TableCell>
+                <TableCell>
+                  <UserActionButtons />
+                </TableCell>
+              </TableRow>
+            );
+          })}
         </TableBody>
       </Table>
       <div className="mt-8 flex justify-end">
-        <Pagination totalPage={10} currentPage={1} />
+        <Pagination totalPage={data.totalPage} currentPage={data.currentPage} />
       </div>
     </>
   );
